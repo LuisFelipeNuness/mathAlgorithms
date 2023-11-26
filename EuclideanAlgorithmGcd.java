8074f764bd050706355681c793ae24eccd654c1b
Luis Felipe Nunes <61566892+LuisFelipeNuness@users.noreply.github.com>
2023-11-26 15:19:25 -0300

    Update and rename mdc.java to EuclideanAlgorithmGcd.java

--- EuclideanAlgorithmGcd.java
+++ EuclideanAlgorithmGcd.java
@@ -1,26 +1,26 @@
 import java.util.Scanner;
 
-public class Main
+public class EuclideanAlgorithm
 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int num1 = sc.nextInt();
 		int num2 = sc.nextInt();
 		
-		int mdc = mdc(num1, num2);
+		int gcd = gcd(num1, num2);
 		
 		System.out.println(mdc);
 	}
 	
-	public static int mdc(int num1, int num2){
-	    int resto = 0;
+	public static int gcd(int num1, int num2){
+	    int remainder = 0;
 	    while(true){
-	        resto = num1 % num2;
+	        remainder = num1 % num2;
 	        if(resto == 0){
 	            return num2;
 	        }
 	        num1 = num2;
-	        num2 = resto;
+	        num2 = remainder;
 	    } 
 	} 
 }
